8e5b010172e673ecbdb90b08514d97de23783c79
David <quachddavid@gmail.com>
2022-04-01 13:19:37 +1100

    Added draw_text method

--- lib/baccarat_gui.rb
+++ lib/baccarat_gui.rb
@@ -21,4 +21,20 @@ module BaccaratGUI
       title: { top_center: Rainbow("\u039B WELCOME TO THE RUBY \u039B").darkred, bottom_right: 'v(1.0)' }
     )
   end
+
+  def draw_text(position, text, border_on, align, dimensions = [10, 3])
+    puts TTY::Box.frame(
+      left: position[0],
+      top: position[1],
+      width: dimensions[0],
+      height: dimensions[1],
+      align: align,
+      border: {
+        top: border_on,
+        bottom: border_on,
+        left: border_on,
+        right: border_on
+      }
+    ) { text }
+  end
 end
