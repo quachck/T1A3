a12e1b4d3a7f39e47a1b826deb731b002e8df4e5
David <quachddavid@gmail.com>
2022-04-01 14:30:21 +1100

    Added draw_table - the main method that will draw the updated table after each round

--- lib/baccarat_gui.rb
+++ lib/baccarat_gui.rb
@@ -37,4 +37,24 @@ module BaccaratGUI
       }
     ) { text }
   end
+
+  def draw_table(dealer_info, player_result, results, balance, bet_info)
+    system 'clear'
+    draw_text([16, 4], "PLAYER#{' ' * 35}BANKER", false, :left, [50, 3])
+    draw_text([31, 7], "#{results[0]}  #{result.to_s.upcase}!  #{results[1]}", false, :center, [20, 3])
+    draw_text([2, 10], "PLAYER PAYS 1:1\nBANKER PAYS 1:1\nTIE PAYS 8:1", true, :left, [25, 5])
+    draw_text([28, 10], "#{Rainbow("BALANCE: \u039B#{balance}\n").gold}#{Rainbow("BET: \u039B#{bet_info[1]} ON #{bet_info[0].upcase}").blue}", true, :center, [25, 5])
+    draw_text([54, 10], "HISTORY:\n#{dealer_info[2].join(' ')}", true, :left, [25, 5])
+    draw_text([25, 17], player_result, false, :center, [30, 5])
+
+    draw_card([10, 5], dealer_info[0][0])
+    draw_card([17, 5], dealer_info[0][1])
+    draw_card([24, 5], dealer_info[0][2])
+
+    draw_card([50, 5], dealer_info[1][0])
+    draw_card([57, 5], dealer_info[1][1])
+    draw_card([64, 5], dealer_info[1][2])
+
+    draw_border([0, 0], [81, 22])
+  end
 end
