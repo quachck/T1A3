867425a6bb88a4b5d22490277fa0acfe466330ec
David <quachddavid@gmail.com>
2022-04-01 13:16:23 +1100

    Added draw_border method

--- lib/baccarat_gui.rb
+++ lib/baccarat_gui.rb
@@ -10,4 +10,15 @@ module BaccaratGUI
       height: 4
     ) { "#{card.short_hand_value}#{card.suit_icon}" }
   end
+
+  def draw_border(position, dimensions)
+    puts TTY::Box.frame(
+      left: position[0],
+      top: position[1],
+      width: dimensions[0],
+      height: dimensions[1],
+      border: :thick,
+      title: { top_center: Rainbow("\u039B WELCOME TO THE RUBY \u039B").darkred, bottom_right: 'v(1.0)' }
+    )
+  end
 end
